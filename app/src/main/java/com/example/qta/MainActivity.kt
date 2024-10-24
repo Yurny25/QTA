0661e98fd3722604cb399fb8f781afd8cd5edad1
Yurny25 <yurnyy.ladinon@utadeo.edu.co>
2024-10-24 12:58:00 -0500

    Contenido no agregado por errores

--- app/src/main/java/com/example/qta/MainActivity.kt
+++ app/src/main/java/com/example/qta/MainActivity.kt
@@ -1,20 +1,51 @@
 package com.example.qta
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val firstFragment = firstFragment()
+    private val secondFragment = secondFragment()
+    private val thirdFragment = thirdFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val navigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        navigation.setOnNavigationItemSelectedListener { item ->
+            onNavigationItemSelected(item)
         }
+    }
+
+    private fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.firstFragment -> {
+                loadFragment(firstFragment)
+                true
+            }
+            R.id.secondFragment -> {
+                loadFragment(secondFragment)
+                true
+            }
+            R.id.thirdFragment -> {
+                loadFragment(thirdFragment)
+                true
+            }
+            else -> false
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frame_container, fragment)
+        transaction.commit()
     }
 }
